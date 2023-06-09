2e18d47faf446ecf954655fd931e192044842a02
Mathieu Shodo-Studio <135144997+MathieuShodoStudio@users.noreply.github.com>
2023-06-09 16:54:36 +0200

    Introduced Frame class

--- kotlin/junit/src/main/kotlin/com/kata/Bowling.kt
+++ kotlin/junit/src/main/kotlin/com/kata/Bowling.kt
@@ -7,23 +7,86 @@ interface Game {
 }
 
 class BowlingGame : Game {
-    private var score: Int = 0
+    private val framesCompleted: MutableList<Frame> = ArrayList()
+    private var currentFrame: Frame = BowlingFrame()
+    private var previousFrame: Frame? = null
+
     private var bonusesAvailable: Int = 0
 
-    override fun roll(numberOfPins: Int) {
-        score += numberOfPins
+    init {
+        framesCompleted.add(currentFrame)
+    }
 
-        if (bonusesAvailable > 0) {
-            score += numberOfPins
+    override fun roll(numberOfPins: Int) {
+        val frameStatus = currentFrame.roll(numberOfPins)
+
+        if (bonusesAvailable > 0 && previousFrame != null) {
+            previousFrame!!.bonus(numberOfPins)
             bonusesAvailable--
         }
 
-        if (numberOfPins == 10) {
+        if (FrameStatus.COMPLETED == frameStatus) {
+            initializeNextFrame()
+        }
+        else if (FrameStatus.COMPLETED_STRIKE == frameStatus) {
+            initializeNextFrame()
             bonusesAvailable += 2
         }
-        else if (score == 10) {
+        else if (FrameStatus.COMPLETED_SPARE == frameStatus) {
+            initializeNextFrame()
             bonusesAvailable++
         }
+    }
+
+    private fun initializeNextFrame() {
+        previousFrame = currentFrame
+        currentFrame = BowlingFrame()
+        framesCompleted.add(currentFrame)
+    }
+
+    override fun score(): Int {
+        return framesCompleted.stream().mapToInt { frame -> frame.score() }.sum()
+    }
+}
+
+enum class FrameStatus {
+    NO_ROLL_MADE, FIRST_ROLL_MADE, COMPLETED, COMPLETED_SPARE, COMPLETED_STRIKE
+}
+
+interface Frame {
+    fun roll(numberOfPins: Int): FrameStatus
+
+    fun bonus(numberOfPins: Int)
+
+    fun score(): Int
+}
+
+class BowlingFrame: Frame {
+    private var score: Int = 0
+    private var status = FrameStatus.NO_ROLL_MADE
+
+    override fun roll(numberOfPins: Int) : FrameStatus {
+        score += numberOfPins
+
+        status = if (FrameStatus.NO_ROLL_MADE == status && numberOfPins == 10) {
+            FrameStatus.COMPLETED_STRIKE
+        }
+        else if (FrameStatus.FIRST_ROLL_MADE == status) {
+            if (score == 10) {
+                FrameStatus.COMPLETED_SPARE
+            } else {
+                FrameStatus.COMPLETED
+            }
+        }
+        else {
+            FrameStatus.FIRST_ROLL_MADE
+        }
+
+        return status
+    }
+
+    override fun bonus(numberOfPins: Int) {
+        score += numberOfPins
     }
 
     override fun score(): Int {
